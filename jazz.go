6c0a0a5f47b66c6a6989fd35bfd2b5c7923dc685
Vojtěch Kletečka <vojta.kletecka@gmail.com>
2018-10-24 13:15:11 +0200

    [SAAS-65] Introduced simplified api
    Api contains now of three major parts. Exchange/Queue scheme creation, publishing and consuming. Scheme specification is done via YAML string and this way publishing and consuming is simplified to great extent.

--- jazz.go
+++ jazz.go
@@ -1,16 +1,13 @@
-package rabbit
+package jazz
 
 import(
+	"fmt"
+	"gopkg.in/yaml.v2"
 	"github.com/streadway/amqp"
 )
 
 type Connection struct {
 	c *amqp.Connection
-}
-
-type Queue struct {
-	c *amqp.Channel
-	q amqp.Queue
 }
 
 func Connect(dsn string) (*Connection, error) {
@@ -21,41 +18,153 @@ func Connect(dsn string) (*Connection, error) {
 	return &Connection{conn}, nil
 }
 
-func (c *Connection) ConnectToQueue(name string, recreate bool) (*Queue, error) {
+func (c *Connection) CreateScheme(data []byte) (error) {
 	ch, err := c.c.Channel()
 	if err != nil {
-		return nil, err
+		fmt.Println("jsem tu!")
+		return err
 	}
-	q, err := ch.QueueDeclare(
-		name,  // name
-		true,  // durable
-		false, // delete when unused
-		false, // exclusive
-		false, // no-wait
-		nil,   // arguments
-	)
+
+	s := Settings{}
+	err = yaml.Unmarshal(data, &s)
 	if err != nil {
-		return nil, err
+		fmt.Println("jsem tady!")
+		return err
 	}
-	return &Queue{ch, q}, nil
+
+	// Create exchanges according to settings
+	for name, e := range s.Exchanges {
+		err = ch.ExchangeDeclarePassive(name, e.Type, e.Durable, e.Autodelete, e.Internal, e.Nowait, nil)
+		if err != nil {
+			ch, err = c.c.Channel()
+			if err != nil {
+				return err
+			}
+
+			err = ch.ExchangeDeclare(name, e.Type, e.Durable, e.Autodelete, e.Internal, e.Nowait, nil)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// Create queues according to settings
+	for name, q := range s.Queues {
+		_, err := ch.QueueDeclarePassive(name, q.Durable, q.Autodelete, q.Exclusive, q.Nowait, nil)
+		if err != nil {
+			ch, err = c.c.Channel()
+			if err != nil {
+				return err
+			}
+
+			_, err := ch.QueueDeclare(name, q.Durable, q.Autodelete, q.Exclusive, q.Nowait, nil)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// Create bindings now that everything is setup
+	for name, e := range s.Exchanges {
+		for _, b := range e.Bindings {
+			fmt.Println(b.Exchange, b.Key)
+			err = ch.ExchangeBind(name, b.Key, b.Exchange, b.Nowait, nil)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	for name, q := range s.Queues {
+		for _, b := range q.Bindings {
+			err = ch.QueueBind(name, b.Key, b.Exchange, b.Nowait, nil)
+		}
+	}
+
+	ch.Close()
+	return nil
+}
+
+func (c *Connection) DeleteScheme(data []byte) (error) {
+	ch, err := c.c.Channel()
+	if err != nil {
+		return err
+	}
+
+	s := Settings{}
+	err = yaml.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+
+	for name, _ := range s.Exchanges {
+		err = ch.ExchangeDelete(name, false, false)
+		if err != nil {
+			return err
+		}
+	}
+
+	for name, _ := range s.Queues {
+		_, err = ch.QueueDelete(name, false, false, false)
+		if err != nil {
+			return err
+		}
+	}
+	ch.Close()
+	return nil
 }
 
 func (c *Connection) Close() (error) {
 	return c.c.Close()
 }
 
-func (q *Queue) Close() (error) {
-	return q.c.Close()
-}
+func (c *Connection) SendMessage(ex, key, msg string) (error) {
+	ch, err := c.c.Channel()
+	if err != nil {
+		return err
+	}
 
-func (q *Queue) SendMessage(msg string) (error) {
-	return q.c.Publish("",
-		q.q.Name, // routing key
-		false,    // mandatory
-		false,
+	err = ch.Publish(ex, key, false, false,
 		amqp.Publishing{
 			DeliveryMode: amqp.Persistent,
 			ContentType:  "text/plain",
 			Body:         []byte(msg),
 		})
+	if err != nil {
+		return err
+	}
+	return ch.Close()
+}
+
+func (c *Connection) SendBlob(ex, key string, msg []byte) (error) {
+	ch, err := c.c.Channel()
+	if err != nil {
+		return err
+	}
+
+	err = ch.Publish(ex, key, false, false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "application/octet-stream",
+			Body:         msg,
+		})
+	if err != nil {
+		return err
+	}
+	return ch.Close()
+}
+
+func (c *Connection) ProcessQueue(name string, f func([]byte)) error {
+	ch, err := c.c.Channel()
+	if err != nil {
+		return err
+	}
+	msgs, err := ch.Consume(name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+	for d := range msgs {
+		f(d.Body)
+	}
+	return nil
 }
--- jazz_test.go
+++ jazz_test.go
@@ -1,21 +1,132 @@
-package rabbit
+package jazz
 
 import(
+	"fmt"
 	"testing"
 )
 
+var dsn = "amqp://guest:guest@localhost:5672/"
+
+
+var data = []byte(`
+exchanges:
+  change:
+    durable: true
+    type: topic
+  change1:
+    durable: true
+    type: topic
+    bindings:
+      - exchange: "change"
+        key: "key1"
+      - exchange: "change"
+        key: "key2"
+  change2:
+    durable: true
+    type: topic
+    bindings:
+      - exchange: "change"
+        key: "key3"
+      - exchange: "change1"
+        key: "key2"
+  change3:
+    durable: true
+    type: topic
+    bindings:
+      - exchange: "change"
+        key: "key4"
+queues:
+  queue1:
+    durable: true
+    bindings:
+      - exchange: "change"
+        key: "key4"
+  queue2:
+    durable: true
+    bindings:
+      - exchange: "change1"
+        key: "key2"
+  queue3:
+    durable: true
+    bindings:
+      - exchange: "change1"
+        key: "#"
+  queue4:
+    durable: true
+    bindings:
+      - exchange: "change2"
+        key: "#"
+  queue5:
+    durable: true
+    bindings:
+      - exchange: "change3"
+        key: "#"
+  queue6:
+    durable: true
+    bindings:
+      - exchange: "change"
+        key: "#"
+`)
+
 func TestConnection(t *testing.T) {
-	conn, err := Connect("amqp://guest:guest@localhost:5672/")
+	c, err := Connect(dsn)
 	if err != nil {
-		t.Errorf("Could not connect to rabbitmq")
+		t.Errorf("Could not connect to RabbitMQ: %v", err.Error())
 		return
 	}
-	q, err := conn.ConnectToQueue("pokusna", false)
+	c.Close()
+}
+
+func TestSchemeCreation(t *testing.T) {
+	c, err := Connect(dsn)
 	if err != nil {
-		t.Errorf("Could not connect to rabbitmq")
+		t.Errorf("Could not connect to RabbitMQ: %v", err.Error())
 		return
 	}
-	q.SendMessage("Hello World!")
-	q.Close()
-	conn.Close()
+	err = c.DeleteScheme(data)
+	if err != nil {
+		t.Errorf("Could not create scheme: %v", err.Error())
+		return
+	}
+	err = c.CreateScheme(data)
+	if err != nil {
+		t.Errorf("Could not create scheme: %v", err.Error())
+		return
+	}
+	err = c.DeleteScheme(data)
+	if err != nil {
+		t.Errorf("Could not create scheme: %v", err.Error())
+		return
+	}
+	c.Close()
+}
+
+func TestSendMessage(t *testing.T) {
+	c, err := Connect(dsn)
+	if err != nil {
+		t.Errorf("Could not connect to RabbitMQ: %v", err.Error())
+		return
+	}
+	err = c.CreateScheme(data)
+	if err != nil {
+		t.Errorf("Could not create scheme: %v", err.Error())
+		return
+	}
+
+	f := func(msg []byte) {
+		fmt.Println(string(msg))
+	}
+
+	go c.ProcessQueue("queue1", f)
+	go c.ProcessQueue("queue2", f)
+	go c.ProcessQueue("queue3", f)
+	go c.ProcessQueue("queue4", f)
+	go c.ProcessQueue("queue5", f)
+	go c.ProcessQueue("queue6", f)
+	c.SendMessage("change", "key1", "Hello World!")
+	c.SendMessage("change", "key2", "Hello!")
+	c.SendMessage("change", "key3", "World!")
+	c.SendMessage("change", "key4", "Hi!")
+	c.SendMessage("change", "key5", "Again!")
+	c.Close()
 }
